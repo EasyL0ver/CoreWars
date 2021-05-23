123a5e0278da778680c1d5aa699662796f013fd5
Paweł Kudłacik <pakudlacik@gmail.com>
2021-05-23 23:26:10 +0200

    Add proper module for prisoner dilemma game

--- CoreWars.WebApp/Startup.cs
+++ CoreWars.WebApp/Startup.cs
@@ -14,6 +14,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
+using PrisonerDilemma;
 
 namespace CoreWars.WebApp
 {
@@ -96,6 +97,7 @@ namespace CoreWars.WebApp
 
             builder.RegisterModule<PythonScriptingModule>();
             builder.RegisterModule<DummyCompetitionModule>();
+            builder.RegisterModule<DilemmaModule>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
---
+++ Modules/Games/PrisonerDilemma/DilemmaModule.cs
@@ -0,0 +1,31 @@
+using System.Collections.Generic;
+using Akka.Actor;
+using CoreWars.Common;
+using CoreWars.Competition;
+
+namespace PrisonerDilemma
+{
+    public class DilemmaModule : CompetitionModule
+    {
+        private class DilemmaFactory : ICompetitionActorPropsFactory
+        {
+            public Props Build(IEnumerable<GeneratedAgent> competitionParticipants)
+            {
+                return Props.Create(() =>
+                    new DilemmaCompetitionActor(competitionParticipants, DilemmaConfiguration.Default()));
+            }
+        }
+        
+        protected override ICompetitionActorPropsFactory ConfigureFactory()
+        {
+            return new DilemmaFactory();
+        }
+
+        protected override void ConfigureCompetitionInfo(CompetitionInfo competitionInfo)
+        {
+            competitionInfo.Name = "prisoner-dilemma";
+            competitionInfo.PlayerCount = Range<int>.Between(2,2);
+            competitionInfo.MaxInstancesCount = 1;
+        }
+    }
+}
