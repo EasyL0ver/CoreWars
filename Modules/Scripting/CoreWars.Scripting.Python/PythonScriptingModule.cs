b0667a431cbcb310d9f5211efa6878c41c193c87
Paweł Kudłacik <pakudlacik@gmail.com>
2021-07-22 15:08:31 +0200

    Import ironpython modules via config string

--- Modules/Scripting/CoreWars.Scripting.Python/PythonScriptingModule.cs
+++ Modules/Scripting/CoreWars.Scripting.Python/PythonScriptingModule.cs
@@ -1,15 +1,32 @@
+using System.IO;
 using Autofac;
 using CoreWars.Common;
+using JetBrains.Annotations;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Scripting.Hosting;
 
 namespace CoreWars.Scripting.Python
 {
+    [UsedImplicitly]
     public class PythonScriptingModule : Module
     {
         protected override void Load(ContainerBuilder builder)
         {
             builder
-                .Register(context => IronPython.Hosting.Python.CreateEngine())
+                .Register(context =>
+                {
+                    var config = context.Resolve<IConfiguration>();
+                    var ironPythonPath = config["IronPythonPath"];
+                    var libraryPath = Path.Combine(ironPythonPath, "Lib");
+                    
+                    var engine = IronPython.Hosting.Python.CreateEngine();
+
+                    var engineSearchPaths = engine.GetSearchPaths();
+                    engineSearchPaths.Add(libraryPath);
+                    engine.SetSearchPaths(engineSearchPaths);
+
+                    return engine;
+                })
                 .As<ScriptEngine>()
                 .SingleInstance();
             
