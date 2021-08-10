2819097aeae788468b52c84866ce91857b4c1787
Paweł Kudłacik <pakudlacik@gmail.com>
2021-08-10 21:26:27 +0200

    Throttle status information

--- CoreWars.WebApp/Actors/Notification/StatusObserver.cs
+++ CoreWars.WebApp/Actors/Notification/StatusObserver.cs
@@ -56,7 +56,14 @@ namespace CoreWars.WebApp.Actors.Notification
                 _timeoutCancellable.Cancel();
                 _watchedCompetitor = msg.Subject;
                 _watchedCompetitor.Tell(Subscribe.Instance);
-
+                
+                Context.System.Scheduler.ScheduleTellRepeatedly(
+                    TimeSpan.Zero
+                    , TimeSpan.FromSeconds(3)
+                    , Self
+                    , Messages.ScheduleUpdate.Instance
+                    , Self);
+                
                 Become(ListeningForStatus);
             });
             Receive<Messages.IdentityTimeout>(msg =>
@@ -74,23 +81,19 @@ namespace CoreWars.WebApp.Actors.Notification
             {
                 _cache.Wins = msg.Wins;
                 _cache.GamesPlayed = msg.GamesPlayed;
-
-                Self.Tell(Messages.ScheduleUpdate.Instance);
             });
             Receive<CompetitorState>(msg =>
             {
                 _cache.State = msg;
-                Self.Tell(Messages.ScheduleUpdate.Instance);
             });
             Receive<AgentFailureState>(msg =>
             {
                 _cache.Exception = msg.Exception;
-                Self.Tell(Messages.ScheduleUpdate.Instance);
             });
-            Receive<Messages.ScheduleUpdate>(async msg =>
+            Receive<Messages.ScheduleUpdate>(msg =>
             {
                 var updateMessage = _cache.GetMessage();
-                await NotifyUser(_connectionId, updateMessage);
+                NotifyUser(_connectionId, updateMessage).PipeTo(Self);
             });
             ReceiveAny(msg => throw new InvalidOperationException($"Unknown message type received. Received object: {msg}"));
         }
